7f015690dc069a359322db0274a55f6af96a7443
FranciscoSTJohn <fstjohn@udesa.edu.ar>
2022-11-18 11:50:56 -0300

    Seguimiento buscador

--- JS/buscador.js
+++ JS/buscador.js
@@ -1,12 +1,16 @@
 let query = location.search;
 let stringToObject = new URLSearchParams(query); 
 let aBuscar = stringToObject.get('q'); 
+let buscador= document.querySelector('q')
 
 
-let url = `https://api.themoviedb.org/3/search/movie?query=${aBuscar}&api_key=d1e5f3bc2bd3300eb31bc59ece54409b&language=en-US&page=1&include_adult=false`
-console.log(url);
+let url_movies = `https://api.themoviedb.org/3/search/movie?query=${aBuscar}&api_key=d1e5f3bc2bd3300eb31bc59ece54409b&language=en-US&page=1&include_adult=false`
+let url_series = `https://api.themoviedb.org/3/search/tv?query=${aBuscar}&api_key=d1e5f3bc2bd3300eb31bc59ece54409b&language=en-US&page=1&include_adult=false`
+console.log(url_movies);
+console.log(url_series);
 
-fetch(url)
+
+fetch(url_movies)
     .then(function(response){
         return response.json();
     })
@@ -17,13 +21,14 @@ fetch(url)
         let characters = '';
 
         for(let i=0; i<info.length; i++){
-            characters += `<article>
-                                <img src=${info[i].poster_path} alt='' />
-                                <p>Name: ${info[i].title}</p></a>
+            characters += `<article class="peliculaspopulares">
+                                    <img class="imagen" src=https://image.tmdb.org/t/p/w500/${info[i].poster_path} alt='' />
+                                    <p>${info[i].title}</p>
                                 
                             </article>`
         }
         container.innerHTML = characters;
+        document.querySelector("#titulo").innerHTML= `Ha Buscado ${aBuscar}`
 
         
     })
