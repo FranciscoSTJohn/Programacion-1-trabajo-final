6b738707d3a381768f2807196fc79426a6dd3a6a
AZanardiOcampo <azanardiocampo@udesa.edu.ar>
2022-11-18 10:07:23 -0300

    Generos java

--- JS/generos.js
+++ JS/generos.js
@@ -1,2 +1,17 @@
 let url= `https://api.themoviedb.org/3/genre/movie/list?api_key=d1e5f3bc2bd3300eb31bc59ece54409b&language=en-US`
 console.log(url);
+
+fetch(url)
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+  console.log(data);
+})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
+
+.then(function(url) {
+return
+})
