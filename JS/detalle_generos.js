9fdeeaf523cd6caa63a98ca4b53564c88ab2d9fd
AZanardiOcampo <azanardiocampo@udesa.edu.ar>
2022-11-22 17:57:33 -0300

    Avance, falta terminar

--- JS/detalle_generos.js
+++ JS/detalle_generos.js
@@ -1,10 +1,53 @@
-let url = `https://api.themoviedb.org/3/discover/movie?api_key=<<api_key>>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate` 
-let url2= `https://api.themoviedb.org/3/discover/tv?api_key=<<api_key>>&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
+let url = `https://api.themoviedb.org/3/discover/movie?api_key=d1e5f3bc2bd3300eb31bc59ece54409b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate` 
+let url2= `https://api.themoviedb.org/3/discover/tv?api_key=d1e5f3bc2bd3300eb31bc59ece54409b&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
 fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+        let info = data.results
+        let container = document.querySelector('#generos');
+        let characters = '';
+        let id = info.genre_ids
+        ids = localStorage.getItem("ids");
+        console.log(ids)
+        console.log(id)
+
+        for(let i=0; i<info.length; i++){
+            if (id in ids){
+            characters += `<p>${info[i].id}</p>
+                            <br>`
+            }
+            else 
+            {
+                alert("Error, el genero no existe")
+            }
+
+        } // que lea 1 por 1 y si el id es = que la ponga
+        
+        container.innerHTML = characters;
+    })
+
+    fetch(url2)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
         console.log(data)
+        let container = document.querySelector('#generos');
+        let characters = '';
 
+        for(let i=0; i<info.length; i++){
+            characters += `<article class="peliculaspopulares">
+                                <p><a href= "${info[i].id}" >${info[i].name}</a></p>
+                                <br><br> <br><br>
+                            </article>`
+        }
+        
+        container.innerHTML = characters;
+    })
+
+    .catch(function(error){
+        console.log(error);
     })
--- JS/generos.js
+++ JS/generos.js
@@ -10,8 +10,12 @@ fetch(url)
         let info = data.genres
         let container = document.querySelector('#generos');
         let characters = '';
+        let ids = []
 
         for(let i=0; i<info.length; i++){
+           let id = info[i].id
+           ids.push(id) 
+
             characters += `<article class="peliculaspopulares">
                                 <p><a href= "" >${info[i].name}</a></p>
                                 <br><br> <br><br>
@@ -21,6 +25,64 @@ fetch(url)
         container.innerHTML = characters;
 
     })
+    .catch(function(error){
+        console.log(error);
+    })
+
+
+// Detalles
+
+    //Peliculas
+
+    let url1 = `https://api.themoviedb.org/3/discover/movie?api_key=d1e5f3bc2bd3300eb31bc59ece54409b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate` 
+let url2= `https://api.themoviedb.org/3/discover/tv?api_key=d1e5f3bc2bd3300eb31bc59ece54409b&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
+fetch(url1)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+        let info = data.results
+        let container1 = document.querySelector('#generos');
+        let texto = '';
+        let id1 = info.genre_ids
+                        
+        for(let i=0; i<info.length; i++){
+            if (id1 in ids){
+            texto += `<p>${info[i].id}</p>
+                            <br>`
+            }
+            else 
+            {
+                alert("Error, el genero no existe")
+            }
+
+        } // que lea 1 por 1 y si el id es = que la ponga
+        
+        container1.innerHTML = texto;
+    })
+
+    //Series
+
+    fetch(url2)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        let container = document.querySelector('#generos');
+        let characters = '';
+
+        for(let i=0; i<info.length; i++){
+            characters += `<article class="peliculaspopulares">
+                                <p><a href= "${info[i].id}" >${info[i].name}</a></p>
+                                <br><br> <br><br>
+                            </article>`
+        }
+        
+        container.innerHTML = characters;
+    })
+
     .catch(function(error){
         console.log(error);
     })
